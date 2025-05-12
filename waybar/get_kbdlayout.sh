ba3009929cc37514c9cc5966fa4963b8651d8c4c
dok39 <arvidhsm@gmail.com>
2025-05-13 00:41:14 +0100

    fixed keyboard layout and made some general fixes

--- waybar/get_kbdlayout.sh
+++ waybar/get_kbdlayout.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-language=$(hyprctl devices | grep "active_keymap" | awk '{print $2}')
-echo $language
+language=$(hyprctl devices | grep "active keymap" | awk '{print $3, $4}')
+if [ "$language!=$previous_language" ];then 
+echo "$language"
+previous_language="$language"
+fi
