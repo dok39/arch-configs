525c6e68582ec10d8b20d07921b6b79f8d14e5b4
dok39 <arvidhsm@gmail.com>
2025-05-18 22:48:50 +0100

    colour fixes

--- waybar/get_kbdlayout.sh
+++ waybar/get_kbdlayout.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-language=$(hyprctl devices | grep "active keymap" | awk '{print $3, $4}')
-if [ "$language!=$previous_language" ];then 
-echo "$language"
-previous_language="$language"
-fi
+
+layout=$(hyprctl devices | grep "active keymap" | awk '{print $3 " " $4}' | sort | uniq | head -n1)
+
+echo $layout
