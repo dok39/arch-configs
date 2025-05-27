8c9d706e599c0738dfab2c26f0883ce5628f546e
dok39 <arvidhsm@gmail.com>
2025-05-27 02:46:21 +0100

    fully developed script

--- waybar/swh_wallpaper.sh
+++ waybar/swh_wallpaper.sh
@@ -6,10 +6,21 @@ LIST_FILE="$HOME/.cache/wallpaper_list"
 
 find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" \) | sort > "$LIST_FILE"
 
-TOTAL=$(wc -l < "LIST_FILE")
+TOTAL=$(wc -l < "$LIST_FILE")
 if [ "$TOTAL" -eq 0 ]; then
     echo "Wallpaper folder is empty"
     exit 1
 fi
 
-else
+    [ ! -f "$INDEX_FILE" ] && echo 0 > "$INDEX_FILE"
+
+    INDEX=$(<"$INDEX_FILE")
+if [[ ! "$INDEX" =~ ^[0-9]+$ ]] then 
+    INDEX=0
+fi
+
+    WALLPAPER=$(sed -n "$((INDEX + 1))p" "$LIST_FILE")
+
+    swww img "$WALLPAPER"
+
+    echo $(( (INDEX + 1) % TOTAL )) > "$INDEX_FILE"
