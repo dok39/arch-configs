f94bdf3327e09ea59c1a2bcd0c74c0820842ac3f
dok39 <arvidhsm@gmail.com>
2025-05-27 01:39:07 +0100

    waybar fix and new sh file creation

--- waybar/get_kbdlayout.sh
+++ waybar/get_kbdlayout.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-layout=$(hyprctl devices | grep "active keymap" | awk '{print $3 " " $4}' | sort | uniq | head -n1)
+layout=$(hyprctl devices | grep "active keymap" | awk '{print $3}' | sort | uniq | head -n1)
 
 echo $layout
